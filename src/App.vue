cdc6237d30c1cb9bd52452767c3803a8f218580c
david <davidwilliams_canada@yahoo.com>
2019-04-09 20:10:52 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+ <div>
+   <Input/>
+ </div>
+  </div>
+</template>
+
+<script>
+import Input from '@/components/Input'
+
+
+export default {
+  components: {
+   Input,
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" v-on:input="cssClass = $event.target.value"> 
+    <p v-bind:class="cssClass">Type in a colour to change my background: {{ cssClass }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return{
+      cssClass: ''
+      }
+    }
+ }
+</script>
+
+
+<style>
+.red{
+  background-color: red;
+}
+.blue{
+  background-color: blue;
+}
+.green{
+  background-color: green;
+}
+.yellow{
+  background-color: yellow;
+}
+
+</style>
